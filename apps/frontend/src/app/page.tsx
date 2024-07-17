507bef3e33ca4bb7e5e3b251964ac7e621a23e16
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-17 13:54:39 +0200

    feat(frontend): added fire to frontend page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome frontend 👋 🚀
+              Welcome frontend 👋 🚀 🔥
             </h1>
           </div>
 
