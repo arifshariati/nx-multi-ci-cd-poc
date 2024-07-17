25f2051894aa3a333c4232862a161b789ad34c17
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-17 14:15:53 +0200

    fix(core): added few dependencies and cicd

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome frontend 👋
+              Welcome frontend 👋 🚀 🔥
             </h1>
           </div>
 
